1580b16c5465be553a8b9c35eebb37d9395ffb9b
Shubham Dash <shubham.ddash@gmail.com>
2019-07-20 01:22:33 -0700

    Added Pytorch dataset capabilities

--- vae.py
+++ vae.py
@@ -41,3 +41,68 @@ class VAE(nn.Module):
         mu, logvar = self.encode(x.view(-1, 784))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
+    
+def train(epoch):
+    # toggle model to train mode
+    model.train()
+    train_loss = 0
+    # in the case of MNIST, len(train_loader.dataset) is 60000
+    # each `data` is of BATCH_SIZE samples and has shape [128, 1, 28, 28]
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = Variable(data)
+        if CUDA:
+            data = data.cuda()
+        optimizer.zero_grad()
+
+        # push whole batch of data through VAE.forward() to get recon_loss
+        recon_batch, mu, logvar = model(data)
+        # calculate scalar loss
+        loss = loss_function(recon_batch, data, mu, logvar)
+        # calculate the gradient of the loss w.r.t. the graph leaves
+        # i.e. input variables -- by the power of pytorch!
+        loss.backward()
+        train_loss += loss.data[0]
+        optimizer.step()
+        if batch_idx % LOG_INTERVAL == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.data[0] / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+def test(epoch):
+    # toggle model to test / inference mode
+    model.eval()
+    test_loss = 0
+
+    # each data is of BATCH_SIZE (default 128) samples
+    for i, (data, _) in enumerate(test_loader):
+        if CUDA:
+            # make sure this lives on the GPU
+            data = data.cuda()
+
+        # we're only going to infer, so no autograd at all required: volatile=True
+        data = Variable(data, volatile=True)
+        recon_batch, mu, logvar = model(data)
+        test_loss += loss_function(recon_batch, data, mu, logvar).data[0]
+        if i == 0:
+          n = min(data.size(0), 8)
+          # for the first 128 batch of the epoch, show the first 8 input digits
+          # with right below them the reconstructed output digits
+          comparison = torch.cat([data[:n],
+                                  recon_batch.view(BATCH_SIZE, 1, 28, 28)[:n]])
+          save_image(comparison.data.cpu(),
+                     'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+
+if __name__ == "__main__":
+    EPOCHS = 10
+    for epoch in range(EPOCHS):
+        train(epoch)
+        test(epoch)
